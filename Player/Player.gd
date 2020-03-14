e24bed0fb70b7f2df3d569ab97966a4aece6496c
mariothedog <mariothedogmc@gmail.com>
2020-03-14 07:56:21 +0000

    Improved top collision detection

--- Player/Player.gd
+++ Player/Player.gd
@@ -12,11 +12,12 @@ var scale_dir = Vector2(1, 1)
 var jump = false
 
 onready var original_polygon = $CollisionPolygon2D.polygon
+onready var original_polygon_top = $"Top Area2D/CollisionPolygon2D".polygon
 
 func _physics_process(delta):
 	scale_dir = Vector2(1, 1)
 	input()
-	update_collision_shape()
+	update_collision_shapes()
 	movement(delta)
 	animate()
 
@@ -36,8 +37,6 @@ func input():
 	
 	if not (colliding_with_object_up and scale_dir.y > $Sprite.scale.y):
 		$Sprite.scale = lerp($Sprite.scale, scale_dir, 0.15)
-		$"Top Area2D/CollisionShape2D".shape.extents.x = 160 * $Sprite.scale.x
-		$"Top Area2D".position = Vector2(0, -300) * $Sprite.scale
 	
 	# Moving
 	var input_vel_x = 0
@@ -53,13 +52,19 @@ func input():
 	if Input.is_action_pressed("jump"):
 		jump = true
 
-func update_collision_shape():
+func update_collision_shapes():
 	if Vector2(stepify($Sprite.scale.x, 0.01), stepify($Sprite.scale.y, 0.01)) != scale_dir: # So it's not unnecessarily run.
 		for i in range(len($CollisionPolygon2D.polygon)):
 			$CollisionPolygon2D.polygon[i].x = original_polygon[i].x * $Sprite.scale.x
 			$CollisionPolygon2D.polygon[i].y = original_polygon[i].y * $Sprite.scale.y
 		
 		$CollisionPolygon2D.position = Vector2(0, -156) * $Sprite.scale # So the centre of enlargement is at the bottom of the middle of the sprite.
+		
+		for i in range(len($"Top Area2D/CollisionPolygon2D".polygon)):
+			$"Top Area2D/CollisionPolygon2D".polygon[i].x = original_polygon_top[i].x * $Sprite.scale.x
+			$"Top Area2D/CollisionPolygon2D".polygon[i].y = original_polygon_top[i].y * $Sprite.scale.y
+		
+		$"Top Area2D".position = Vector2(0, -300) * $Sprite.scale
 
 func movement(delta):
 	velocity.y += GRAVITY * delta
