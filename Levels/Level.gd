411187562336fc12089472379a9f6c08c3e4b43c
mariothedog <mariothedogmc@gmail.com>
2020-03-17 21:44:18 +0000

    Add a bottom camera limit

--- Levels/Level.gd
+++ Levels/Level.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 export var level_width = 2678
 export var level_ceiling = -578
+export var level_floor = 1472
 
 func _ready():
 	$Player/Camera2D.limit_right = level_width
 	$Player/Camera2D.limit_top = level_ceiling
+	$Player/Camera2D.limit_bottom = level_floor
