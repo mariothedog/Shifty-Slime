85f7db7ea29755a88ae22cba385ed37c2b150fab
mariothedog <mariothedogmc@gmail.com>
2020-03-17 16:16:22 +0000

    Improved the blades of grass

--- Levels/Level0.gd
+++ Levels/Level0.gd
@@ -7,24 +7,26 @@ func _ready():
 		var tile_type = $TileMap.get_cellv(cell)
 		if tile_type == 0: # Grass tile
 			var pos = $TileMap.map_to_world(cell)
-			for i in range(-1, 2):
-				var offset = Vector2(32 + 16*i, 8)
-				var grass_blade = grass_blade_scene.instance()
-				grass_blade.position = pos + offset
-				
-				var random_scale = rand_range(0.15, 0.5)
-				grass_blade.scale.y = random_scale
-				
-				grass_blade.material = grass_blade.material.duplicate()
-				var default_sway_amount = grass_blade.material.get_shader_param("sway_amount")
-				grass_blade.material.set_shader_param("sway_amount", default_sway_amount * random_scale * 4)
-				
-				var default_speed = grass_blade.material.get_shader_param("speed")
-				grass_blade.material.set_shader_param("speed", default_speed * random_scale * 2)
-				
-				if randf() < 0.5:
-					grass_blade.material.set_shader_param("dir", -1.0)
-				
-				grass_blade.material.set_shader_param("colour_multi", rand_range(0.9, 1.1))
-				
-				$"Grass Blades".add_child(grass_blade)
+			for _i in range(2):
+				for j in range(-2, 3):
+					var offset = Vector2(32 + 12*j, rand_range(7, 14))
+					var grass_blade = grass_blade_scene.instance()
+					grass_blade.position = pos + offset
+					
+					var random_scale = rand_range(0.15, 0.5)
+					grass_blade.scale.y = random_scale
+					
+					grass_blade.material = grass_blade.material.duplicate()
+					var default_sway_amount = grass_blade.material.get_shader_param("sway_amount")
+					grass_blade.material.set_shader_param("sway_amount", default_sway_amount * random_scale * 4)
+					
+					var default_speed = grass_blade.material.get_shader_param("speed")
+					grass_blade.material.set_shader_param("speed", default_speed * random_scale * 2)
+					
+					if randf() < 0.5:
+						grass_blade.material.set_shader_param("dir", -1.0)
+						grass_blade.scale.x *= -1
+					
+					grass_blade.material.set_shader_param("colour_multi", rand_range(0.9, 1.1))
+					
+					$"Grass Blades".add_child(grass_blade)
