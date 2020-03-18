370960ca42775cb75fd8f2ef0f357e10ff904fe1
mariothedog <mariothedogmc@gmail.com>
2020-03-18 11:25:19 +0000

    Added a death animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,7 @@ const moving_rotation_normal = 2
 const moving_rotation_short = 0.5
 const moving_rotation_tall = 4
 const GRAVITY = 1200
+const death_split_anim_duration = 0.1
 
 var velocity = Vector2()
 
@@ -36,8 +37,8 @@ onready var right_area_default_pos = $"Detectors/Right Area2D/Right".position
 onready var eyes_default_pos = $Eyes.position
 onready var eye_default_pos = $"Eyes/Right Eye Base".position
 
-onready var texture_height = $Sprite.texture.get_height()
-onready var texture_width = $Sprite.texture.get_width()
+onready var texture_height = $"Sprites/Left Sprite".texture.get_height()
+onready var texture_width = $"Sprites/Left Sprite".texture.get_width() * 2
 onready var bottom_offset = Vector2(0, -texture_height/2)
 onready var left_offset = Vector2(texture_width/2, -texture_height/2)
 onready var right_offset = Vector2(-texture_width/2, -texture_height/2)
@@ -64,17 +65,20 @@ func _get_input():
 	
 	var can_scale = true
 	if (($"Detectors/Left Area2D".get_overlapping_bodies() and $"Detectors/Right Area2D".get_overlapping_bodies()) or
-	($"Detectors/Top Area2D".get_overlapping_bodies() and scale_dir.y > $Sprite.scale.y)):
+	($"Detectors/Top Area2D".get_overlapping_bodies() and scale_dir.y > $Sprites.scale.y)):
 		can_scale = false
 	elif $"Detectors/Left Area2D".get_overlapping_bodies():
-		$Sprite.offset = left_offset
-		$Sprite.position.x = -left_offset.x * $Sprite.scale.x
+		$"Sprites/Left Sprite".offset = left_offset
+		$"Sprites/Right Sprite".offset = left_offset
+		$Sprites.position.x = -left_offset.x * $Sprites.scale.x
 	elif $"Detectors/Right Area2D".get_overlapping_bodies():
-		$Sprite.offset = right_offset
-		$Sprite.position.x = -right_offset.x * $Sprite.scale.x
+		$"Sprites/Left Sprite".offset = right_offset
+		$"Sprites/Right Sprite".offset = right_offset
+		$Sprites.position.x = -right_offset.x * $Sprites.scale.x
 	else:
-		$Sprite.offset = bottom_offset
-		$Sprite.position.x = 0
+		$"Sprites/Left Sprite".offset = bottom_offset
+		$"Sprites/Right Sprite".offset = bottom_offset
+		$Sprites.position.x = 0
 	
 	if can_scale and not jump_held and not $Tween.is_active():
 		# Default animation
@@ -83,35 +87,35 @@ func _get_input():
 		scale_dir.x = clamp(scale_dir.x - scale, 0.01, 1.99)
 		
 		# Expand/shrink the player
-		$Sprite.scale = lerp($Sprite.scale, scale_dir, 0.15)
+		$Sprites.scale = lerp($Sprites.scale, scale_dir, 0.15)
 	
-	$Camera2D.position = lerp($Camera2D.position, Vector2(0, -200) * $Sprite.scale.y, 0.15)
+	$Camera2D.position = lerp($Camera2D.position, Vector2(0, -200) * $Sprites.scale.y, 0.15)
 	
 	# Moving
 	var input_vel_x = 0
 	if Input.is_action_pressed("move_right"):
 		input_vel_x += 1
 		
-		if $Sprite.scale.x > 1.1:
+		if $Sprites.scale.x > 1.1:
 			rotation_degrees = lerp(rotation_degrees, moving_rotation_short, 0.15)
-		elif $Sprite.scale.x < 0.9:
+		elif $Sprites.scale.x < 0.9:
 			rotation_degrees = lerp(rotation_degrees, moving_rotation_tall, 0.15)
 		else:
 			rotation_degrees = lerp(rotation_degrees, moving_rotation_normal, 0.15)
 		
-		$Eyes.position.x = lerp($Eyes.position.x, eye_default_pos.x * $Sprite.scale.x, 0.2)
+		$Eyes.position.x = lerp($Eyes.position.x, eye_default_pos.x * $Sprites.scale.x, 0.2)
 	
 	if Input.is_action_pressed("move_left"):
 		input_vel_x -= 1
 		
-		if $Sprite.scale.x > 1.1:
+		if $Sprites.scale.x > 1.1:
 			rotation_degrees = lerp(rotation_degrees, -moving_rotation_short, 0.15)
-		elif $Sprite.scale.x < 0.9:
+		elif $Sprites.scale.x < 0.9:
 			rotation_degrees = lerp(rotation_degrees, -moving_rotation_tall, 0.15)
 		else:
 			rotation_degrees = lerp(rotation_degrees, -moving_rotation_normal, 0.15)
 		
-		$Eyes.position.x = lerp($Eyes.position.x, -eye_default_pos.x * $Sprite.scale.x, 0.2)
+		$Eyes.position.x = lerp($Eyes.position.x, -eye_default_pos.x * $Sprites.scale.x, 0.2)
 	
 	if input_vel_x == 0:
 		rotation_degrees = lerp(rotation_degrees, 0, 0.15)
@@ -123,7 +127,7 @@ func _get_input():
 	jump = false
 	if Input.is_action_pressed("jump"):
 		jump_held = true
-		$Sprite.scale = lerp($Sprite.scale, Vector2(1 + expanding_scale, 1 - expanding_scale), 0.08)
+		$Sprites.scale = lerp($Sprites.scale, Vector2(1 + expanding_scale, 1 - expanding_scale), 0.08)
 	
 	if Input.is_action_just_released("jump"):
 		jump_held = false
@@ -134,35 +138,35 @@ func _get_input():
 		global.restart_level()
 
 func _update_collision_shapes():
-	if stepify_vector($Sprite.scale, 0.01) != scale_dir: # So it's not unnecessarily run.
+	if stepify_vector($Sprites.scale, 0.01) != scale_dir: # So it's not unnecessarily run.
 		for i in range(len($CollisionPolygon2D.polygon)):
-			$CollisionPolygon2D.polygon[i].x = original_polygon[i].x * $Sprite.scale.x
-			$CollisionPolygon2D.polygon[i].y = original_polygon[i].y * $Sprite.scale.y
+			$CollisionPolygon2D.polygon[i].x = original_polygon[i].x * $Sprites.scale.x
+			$CollisionPolygon2D.polygon[i].y = original_polygon[i].y * $Sprites.scale.y
 		
-		$CollisionPolygon2D.position = collision_polygon_default_pos * $Sprite.scale # So the centre of enlargement is at the bottom of the middle of the sprite.
+		$CollisionPolygon2D.position = collision_polygon_default_pos * $Sprites.scale # So the centre of enlargement is at the bottom of the middle of the sprite.
 		
 		for i in range(len($"Detectors/Top Area2D/CollisionPolygon2D".polygon)):
-			$"Detectors/Top Area2D/CollisionPolygon2D".polygon[i].x = original_polygon_top[i].x * $Sprite.scale.x
-			$"Detectors/Top Area2D/CollisionPolygon2D".polygon[i].y = original_polygon_top[i].y * $Sprite.scale.y
+			$"Detectors/Top Area2D/CollisionPolygon2D".polygon[i].x = original_polygon_top[i].x * $Sprites.scale.x
+			$"Detectors/Top Area2D/CollisionPolygon2D".polygon[i].y = original_polygon_top[i].y * $Sprites.scale.y
 		
-		$"Detectors/Top Area2D".position = top_area_default_pos * $Sprite.scale
+		$"Detectors/Top Area2D".position = top_area_default_pos * $Sprites.scale
 		
 		for i in range(len($"Detectors/Left Area2D/Left".polygon)):
-			$"Detectors/Left Area2D/Left".polygon[i].x = original_polygon_left[i].x * $Sprite.scale.x
-			$"Detectors/Left Area2D/Left".polygon[i].y = original_polygon_left[i].y * $Sprite.scale.y
+			$"Detectors/Left Area2D/Left".polygon[i].x = original_polygon_left[i].x * $Sprites.scale.x
+			$"Detectors/Left Area2D/Left".polygon[i].y = original_polygon_left[i].y * $Sprites.scale.y
 		
-		$"Detectors/Left Area2D/Left".position = left_area_default_pos * $Sprite.scale
+		$"Detectors/Left Area2D/Left".position = left_area_default_pos * $Sprites.scale
 		
 		for i in range(len($"Detectors/Right Area2D/Right".polygon)):
-			$"Detectors/Right Area2D/Right".polygon[i].x = original_polygon_right[i].x * $Sprite.scale.x
-			$"Detectors/Right Area2D/Right".polygon[i].y = original_polygon_right[i].y * $Sprite.scale.y
+			$"Detectors/Right Area2D/Right".polygon[i].x = original_polygon_right[i].x * $Sprites.scale.x
+			$"Detectors/Right Area2D/Right".polygon[i].y = original_polygon_right[i].y * $Sprites.scale.y
 		
-		$"Detectors/Right Area2D/Right".position = right_area_default_pos * $Sprite.scale
+		$"Detectors/Right Area2D/Right".position = right_area_default_pos * $Sprites.scale
 		
 		_update_raycasts()
 
 func _movement(delta):
-	velocity.y += GRAVITY * (1 / $Sprite.scale.x) * delta
+	velocity.y += GRAVITY * (1 / $Sprites.scale.x) * delta
 	
 	var vel = move_and_slide(velocity, Vector2.UP)
 	
@@ -177,38 +181,39 @@ func _movement(delta):
 	velocity = vel
 	
 	if is_touching_floor and jump:
-		velocity.y = -jump_speed * pow($Sprite.scale.x, 1.8)
+		velocity.y = -jump_speed * pow($Sprites.scale.x, 1.8)
 
 func _animate(delta):
-	$Eyes.position.y = eyes_default_pos.y * $Sprite.scale.y
+	$Eyes.position.y = eyes_default_pos.y * $Sprites.scale.y
 	
-	# Clamps it to inside the elliptic eye boundary
-	var final_left_eye_pos = ($"Eyes/Left Eye Base".get_local_mouse_position() / Vector2(20, 30)).clamped(1) * Vector2(20, 30)
-	var final_right_eye_pos = ($"Eyes/Right Eye Base".get_local_mouse_position() / Vector2(20, 30)).clamped(1) * Vector2(20, 30)
+	if not dead:
+		# Clamps it to inside the elliptic eye boundary
+		var final_left_eye_pos = ($"Eyes/Left Eye Base".get_local_mouse_position() / Vector2(20, 30)).clamped(1) * Vector2(20, 30)
+		var final_right_eye_pos = ($"Eyes/Right Eye Base".get_local_mouse_position() / Vector2(20, 30)).clamped(1) * Vector2(20, 30)
+		
+		$"Eyes/Left Eye Base/Pupil".position = lerp($"Eyes/Left Eye Base/Pupil".position, final_left_eye_pos, 0.4)
+		$"Eyes/Right Eye Base/Pupil".position = lerp($"Eyes/Right Eye Base/Pupil".position, final_right_eye_pos, 0.4)
+		
+		# Landing animation
+		if not was_on_floor_last_frame and is_touching_floor and impact_velocity.y > 400 and not $Tween.is_active() and time_since_last_landing_anim > 0.4:
+			var final_scale = Vector2(1 + clamp(landing_expanding_scale * impact_velocity.y / 1400, 0.1, 0.9),
+			1 - clamp(landing_expanding_scale * impact_velocity.y / 1400, 0.1, 0.9))
+			
+			if $Sprites.scale.y < final_scale.y:
+				final_scale = Vector2($Sprites.scale.x + 0.15, $Sprites.scale.y - 0.15)
+			
+			$Tween.interpolate_property($Sprites, "scale", $Sprites.scale,
+			final_scale, 200/impact_velocity.y,
+			Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+			
+			$Tween.start()
+			
+			time_since_last_landing_anim = 0
+		else:
+			time_since_last_landing_anim += delta
 	
-	$"Eyes/Left Eye Base/Pupil".position = lerp($"Eyes/Left Eye Base/Pupil".position, final_left_eye_pos, 0.4)
-	$"Eyes/Right Eye Base/Pupil".position = lerp($"Eyes/Right Eye Base/Pupil".position, final_right_eye_pos, 0.4)
-	
-	# Landing animation
-	if not was_on_floor_last_frame and is_touching_floor and impact_velocity.y > 400 and not $Tween.is_active() and time_since_last_landing_anim > 0.4:
-		var final_scale = Vector2(1 + clamp(landing_expanding_scale * impact_velocity.y / 1400, 0.1, 0.9),
-		1 - clamp(landing_expanding_scale * impact_velocity.y / 1400, 0.1, 0.9))
-		
-		if $Sprite.scale.y < final_scale.y:
-			final_scale = Vector2($Sprite.scale.x + 0.15, $Sprite.scale.y - 0.15)
-		
-		$Tween.interpolate_property($Sprite, "scale", $Sprite.scale,
-		final_scale, 200/impact_velocity.y,
-		Tween.TRANS_BOUNCE, Tween.EASE_OUT)
-		
-		$Tween.start()
-		
-		time_since_last_landing_anim = 0
-	else:
-		time_since_last_landing_anim += delta
-	
-	$Sprite.scale.x = clamp($Sprite.scale.x, 0.01, 1.99)
-	$Sprite.scale.y = clamp($Sprite.scale.y, 0.01, 1.99)
+	$Sprites.scale.x = clamp($Sprites.scale.x, 0.01, 1.99)
+	$Sprites.scale.y = clamp($Sprites.scale.y, 0.01, 1.99)
 	
 	was_on_floor_last_frame = is_touching_floor
 
@@ -219,15 +224,15 @@ func _update_raycasts():
 	for raycast in $"Detectors/Floor Detector".get_children():
 			raycast.enabled = true
 	
-	if $Sprite.scale.x <= 1.2:
+	if $Sprites.scale.x <= 1.2:
 		$"Detectors/Floor Detector/RayCast2D6".enabled = false
 		$"Detectors/Floor Detector/RayCast2D7".enabled = false
 	
-	if $Sprite.scale.x <= 0.7:
+	if $Sprites.scale.x <= 0.7:
 		$"Detectors/Floor Detector/RayCast2D4".enabled = false
 		$"Detectors/Floor Detector/RayCast2D5".enabled = false
 	
-	if $Sprite.scale.x <= 0.6:
+	if $Sprites.scale.x <= 0.6:
 		$"Detectors/Floor Detector/RayCast2D2".enabled = false
 		$"Detectors/Floor Detector/RayCast2D3".enabled = false
 
@@ -237,6 +242,35 @@ func _on_Spawning_Particles_Timer_timeout():
 func kill():
 	dead = true
 	
+	velocity.x = 0
+	
 	# Play death animation
-	# TODO
+	$Tween.interpolate_property($"Sprites/Left Sprite", "position",
+	$"Sprites/Left Sprite".position, $"Sprites/Left Sprite".position + Vector2(-25, 0),
+	death_split_anim_duration, Tween.TRANS_BACK, Tween.EASE_OUT, 0.05)
+	$Tween.start()
+	
+	$Tween.interpolate_property($"Sprites/Right Sprite", "position",
+	$"Sprites/Right Sprite".position, $"Sprites/Right Sprite".position + Vector2(25, 0),
+	death_split_anim_duration, Tween.TRANS_BACK, Tween.EASE_OUT, 0.05)
+	$Tween.start()
+	
+	$Tween.interpolate_property($"Eyes/Left Eye Base", "position",
+	$"Eyes/Left Eye Base".position, $"Eyes/Left Eye Base".position + Vector2(-25, 0) * ($Sprites.scale.x * 0.9),
+	death_split_anim_duration, Tween.TRANS_BACK, Tween.EASE_OUT, 0.05)
+	$Tween.start()
+	
+	$Tween.interpolate_property($"Eyes/Right Eye Base", "position",
+	$"Eyes/Right Eye Base".position, $"Eyes/Right Eye Base".position + Vector2(25, 0) * ($Sprites.scale.x * 0.9),
+	death_split_anim_duration, Tween.TRANS_BACK, Tween.EASE_OUT, 0.05)
+	$Tween.start()
+	
+	yield($Tween, "tween_all_completed")
+	
+	$Tween.interpolate_property(self, "modulate",
+	modulate, Color(1, 1, 1, 0), 1)
+	$Tween.start()
+	
+	yield($Tween, "tween_all_completed")
+	
 	global.restart_level()
